dc5b0ef4656dc11833e8680bb07aa5873ece2ca9
Middledeveloper <nikolay.tikhvin@gmail.com>
2020-06-07 12:51:32 +0300

    Models added

---
+++ UserPurses/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserPurses.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public Purse Purse { get; set; }
+        public Currency Currency { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
